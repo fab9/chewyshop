8161605e746b2008738e5822f8a0d9bfc4b62348
fab9 <rorou192@gmail.com>
2015-12-28 14:20:40 -0500

    Make dummy data availble in the view.

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('chewyshopApp')
-  .controller('ProductsCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    // Inject our `Products` factory into the controller.
+    .controller('ProductsCtrl', function ($scope, Products) {
+        // Create new variable `$scope.products` so that `products` become available in the view as well.
+        $scope.products = Products;
+    });
--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -1,16 +1,13 @@
 'use strict';
 
 angular.module('chewyshopApp')
-  .factory('products', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
+  .factory('Product', function () {
+      // Since server is not ready yet, use an array to hold the data.
+      return [
+          {_id: 1, title: 'Blue Buffalo', price: 29.99, quantity: 100, description: 'Lorem ipsum dolor sit amet'},
+          {_id: 2, title: 'Product 2', price: 14.99, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+          {_id: 3, title: 'Product 3', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+          {_id: 4, title: 'Product 4', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor sit amet'},
+          {_id: 5, title: 'Greenies', price: 19.99, quantity: 10, description: 'Lorem ipsum dolor sit amet'}
+      ];
   });
