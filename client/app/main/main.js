5d715e50d9dca1cd28893e721ca883c04a0a68dd
fab9 <rorou192@gmail.com>
2015-12-28 15:07:40 -0500

    Products page looking good so far.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,11 +1,14 @@
+/*
+ Our routes go here
+ */
 'use strict';
 
 angular.module('chewyshopApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('main', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-  });
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('main', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            });
+    });
--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('chewyshopApp')
     // Inject our `Products` factory into the controller.
-    .controller('ProductsCtrl', function ($scope, Products) {
+    .controller('ProductsCtrl', function ($scope, Product) {
         // Create new variable `$scope.products` so that `products` become available in the view as well.
-        $scope.products = Products;
+        $scope.products = Product;
     });
