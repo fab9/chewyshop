b574e6a80af2f6b61ca58dbe083b3d7146bf6b49
fab9 <rorou192@gmail.com>
2015-12-28 19:19:59 -0500

    Server-side file upload setup.

--- server/api/product/index.js
+++ server/api/product/index.js
@@ -2,9 +2,15 @@
 
 var express = require('express');
 var controller = require('./product.controller');
+var multiparty = require('connect-multiparty');
+// For now, store the files locally, not in the cloud.
+var uploadOptions = { autoFile: true,
+    uploadDir: 'client/assets/uploads/'
+}
 
 var router = express.Router();
 
+router.post('/:id/upload', multiparty(uploadOptions), controller.upload);
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -7,7 +7,9 @@ var ProductSchema = new Schema({
     title: { type: String, required: true, trim: true },
     price: { type: Number, required: true, min: 0 },
     stock: { type: Number, default: 1 },
-    description: String
+    description: String,
+    imageBin: { data: Buffer, contentType: String },
+    imageUrl: String // Store the path in the system (or the cloud), where the image is hosted.
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
