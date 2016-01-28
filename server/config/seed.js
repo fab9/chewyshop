361297a195c5957e5ddc2fe6e0940a0fb51ec24e
fab9 <rorou192@gmail.com>
2016-01-28 16:53:52 -0500

    UI enhancements

--- server/config/seed.js
+++ server/config/seed.js
@@ -64,98 +64,98 @@ Catalog
         }, {
             title: 'Unicorn Horn for Horses',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/horse-unicorn-horn.jpg',
-            price: 5.97,
+            price: 15.97,
             stock: 100,
             categories: [other],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Dog Toys Storage',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/dog-toys-burlap-storage.jpg',
-            price: 99.34,
+            price: 19.34,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Breakaway Cat Collar',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-collar-breakaway.jpg',
-            price: 8.00,
+            price: 18.00,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Personalized Mat',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/personalized-pet-mat.jpg',
-            price: 9.34,
+            price: 19.34,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Embossing Rolling Pin',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-embossing-rolling-pin.jpg',
-            price: 5.97,
+            price: 15.97,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Stitched Leather Collar',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/stitched-leather-collar.jpg',
-            price: 9.24,
+            price: 19.24,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Cat Stickers',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-stickers.jpg',
-            price: 7.97,
+            price: 17.97,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Personalized Scarves',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/personalized-scarves.jpg',
-            price: 9.34,
+            price: 19.34,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Heart Cat Tag',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-call-mom.jpg',
-            price: 5.44,
+            price: 15.44,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Stoneware Urn',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/stoneware-pet-urn.jpg',
-            price: 9.44,
+            price: 19.44,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Cat Prints',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-prints.jpg',
-            price: 5.17,
+            price: 15.17,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Lost Tag',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/lost-tag.jpg',
-            price: 9.34,
+            price: 19.34,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Cat Feeder',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/cat-feeder.jpg',
-            price: 4.97,
+            price: 24.97,
             stock: 100,
             categories: [cats],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
         }, {
             title: 'Rope Dog Leash',
             imageUrl: 'https://s3.amazonaws.com/petshop-images/dog-rope-leash.jpg',
-            price: 9.34,
+            price: 19.34,
             stock: 100,
             categories: [dogs],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas.'
